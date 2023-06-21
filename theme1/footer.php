199df793fabe7081fe60397a3e59fc8db7bb12b3
Leart Ademi <65449937+Learti82@users.noreply.github.com>
2023-06-21 11:24:01 +0200

    Add files via upload

---
+++ theme1/footer.php
@@ -0,0 +1,21 @@
+<footer id="colophon" class="site-footer" role="contentinfo">
+  <div class="site-info">
+    &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.
+  </div>
+</footer>
+<?php wp_footer(); ?>
+<style>
+.site-footer {
+  background-color: #f5f5f5;
+  padding: 20px;
+  text-align: center;
+}
+
+.site-info {
+  color: #888;
+  font-size: 14px;
+}
+</style>
+</body>
+
+</html>
